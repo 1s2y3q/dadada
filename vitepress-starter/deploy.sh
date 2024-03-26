28bf2afd83be47e6b3aabe84ea2cb9e7b805b62c
22017365 <sunyuanquan@haier.com>
2024-03-26 11:01:30 +0800

    提交

--- vitepress-starter/deploy.sh
+++ vitepress-starter/deploy.sh
@@ -21,8 +21,8 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# Jacqueline712/mondaylab-blog 替换为自己的用户名和对应的仓库名
-# 意思为将master构建后的代码合并到gh-pages分支上，然后在gh-pages分支上部署~
+# https://github.com/1s2y3q/mondaylab-demo-blog.git 替换为自己的用户名和对应的仓库名
+# 意思为将main构建后的代码在main分支上部署~ sh ./vitepress-starter/deploy.sh
 git push -f https://github.com/1s2y3q/mondaylab-demo-blog.git main
 
 cd -
